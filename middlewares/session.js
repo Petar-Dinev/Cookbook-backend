97f43f3a134b7b84428a636783673fa4a99c9661
Petar Dinev <p.dinev55@gmail.com>
2025-02-22 13:35:11 +0200

    Add session middleware

---
+++ middlewares/session.js
@@ -0,0 +1,20 @@
+const { verifyToken } = require("../services/userService");
+const errorParser = require("../utils/errorParser");
+
+module.exports = () => (req, res, next) => {
+    const token = req.headers['p-authorization'];
+
+    if (token) {
+        try {
+            const userData = verifyToken(token);
+            req.user = userData;
+            req.token = token;
+        } catch (err) {
+            console.log(err);
+            
+            return res.status(403).json({ message: 'Invalid authorization token!' })
+        }
+    }
+
+    next()
+}
