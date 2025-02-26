b18a92b8364dd23151a747f4f665a89ddc92b93d
Petar Dinev <p.dinev55@gmail.com>
2025-02-26 14:24:42 +0200

    Add owner guard

--- middlewares/guards.js
+++ middlewares/guards.js
@@ -1,9 +1,9 @@
 function isGuest() {
     return (req, res, next) => {
         if (!req.user) {
-            next();
+            return next();
         } else {
-            return res.status(403).json({ message: 'You are already logged in!' })
+            return res.status(403).json({ message: 'You are already logged in!' });
         }
     }
 }
@@ -11,20 +11,25 @@ function isGuest() {
 function hasUser() {
     return (req, res, next) => {
         if (req.user) {
-            next();
+            return next();
         } else {
-            return res.status(401).json({ message: 'Pls log in to access this resource!' })
+            return res.status(401).json({ message: 'Pls log in to access this resource!' });
         }
     }
 }
 
-// function isOwner() {
-//     return (req, res, next) => {
+function isOwner() {
+    return (req, res, next) => {
+        if (req.item.owner.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ message: 'You can\'t do that' });
+        }
 
-//     }
-// }
+        return next();
+    }
+}
 
 module.exports = {
     isGuest,
     hasUser,
+    isOwner
 }
