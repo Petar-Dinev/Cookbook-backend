741b1a7b71b93d60653b886297bb679b33eb6649
Petar Dinev <p.dinev55@gmail.com>
2025-02-28 12:37:17 +0200

    Add global not found route

--- configs/routes.js
+++ configs/routes.js
@@ -3,8 +3,10 @@ const recipeController = require('../controllers/recipeController');
 const userController = require('../controllers/userController');
 
 module.exports = (app) => {
-    app.use('/api', homeController)
-    app.use('/api/users', userController)
-    app.use('/api/recipes', recipeController)
-}
-
+    app.use('/api', homeController);
+    app.use('/api/users', userController);
+    app.use('/api/recipes', recipeController);
+    app.use('*', (req, res) => {
+        res.status(404).json({message: '404 Not Found'});
+    });
+};
